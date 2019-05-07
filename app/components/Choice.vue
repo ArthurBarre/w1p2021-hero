670f8891478ba3dfd9e93b79ef134b10d3f40d30
KalaniM <kalakalanicontact@gmail.com>
2019-05-07 09:05:59 +0200

    test push kala

--- app/components/Choice.vue
+++ app/components/Choice.vue
@@ -7,10 +7,12 @@ export default {
   name: "Choice",
   data() {
     return {
-      pops: ["answer"],
+      props: ["answer"],
       data: {
         posts: [
-          { id: 1, title: "Mon initiation avec Vue" },
+          { 
+          "id": 1, 
+          title: "Mon initiation avec Vue" },
           { id: 2, title: "Blogger avec Vue" },
           { id: 3, title: "Pourquoi Vue est tellemen cool" }
         ]
